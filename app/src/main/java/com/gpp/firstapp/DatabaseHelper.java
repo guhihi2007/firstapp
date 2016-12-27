c28d163a47379345ea5c17d3a6fc051096904fac
guhihi2007 <464955343@qq.com>
2016-12-27 17:52:46 +0800

    增加数据库记录输入的用户名密码

---
+++ app/src/main/java/com/gpp/firstapp/DatabaseHelper.java
@@ -0,0 +1,44 @@
+package com.gpp.firstapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import static android.provider.BaseColumns._ID;
+
+/**
+ * Created by Administrator on 2016/12/26.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String TABLE_NAME = "userinfo";
+    public static final int VERSION = 1;//固定版本号
+
+
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public DatabaseHelper(Context context, String name, int version) {
+        this(context, name, null, version);
+    }
+
+    public DatabaseHelper(Context context, String name) {
+        this(context, name, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE IF NOT EXISTS "+TABLE_NAME + " ( name TEXT,password TEXT)";
+        //_id integer primary key autoincrement," + "
+        db.execSQL(sql);
+        Log.d("gpp", "创建完成");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        System.out.print("NO Upgrade!");
+    }
+}
---
+++ app/src/main/java/com/gpp/firstapp/Initialization.java
@@ -0,0 +1,16 @@
+package com.gpp.firstapp;
+
+/**
+ * Created by Administrator on 2016/12/27.
+ */
+
+/**
+ * 初始化View
+ * 初始化监听器
+ */
+interface Initialization {
+
+     void initView();
+
+    void initListener();
+}
